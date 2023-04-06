c8b60a9088cd2fb81dda6f57850ca2ce2d9bd83c
Willyan <wkinhaamaral@gmail.com>
2023-04-06 10:21:57 -0300

    virar carta onclick e limite de duas viradas antes de desvirar

--- script.js
+++ script.js
@@ -1,44 +1,52 @@
-const cards = ["<div class='card'><img class='back-face face' src='/img/bobrossparrot.gif' alt=''><img class='front-face face' src='/img/back.png' alt=''></div>",
-"<div class='card'><img class='back-face face' src='/img/bobrossparrot.gif' alt=''><img class='front-face face' src='/img/back.png' alt=''></div>",
-"<div class='card'><img class='back-face face' src='/img/explodyparrot.gif' alt=''><img class='front-face face' src='/img/back.png' alt=''></div>",
-"<div class='card'><img class='back-face face' src='/img/explodyparrot.gif' alt=''><img class='front-face face' src='/img/back.png' alt=''></div>",
-"<div class='card'><img class='back-face face' src='/img/fiestaparrot.gif' alt=''><img class='front-face face' src='/img/back.png' alt=''></div>",
-"<div class='card'><img class='back-face face' src='/img/fiestaparrot.gif' alt=''><img class='front-face face' src='/img/back.png' alt=''></div>",
-"<div class='card'><img class='back-face face' src='/img/metalparrot.gif' alt=''><img class='front-face face' src='/img/back.png' alt=''></div>",
-"<div class='card'><img class='back-face face' src='/img/metalparrot.gif' alt=''><img class='front-face face' src='/img/back.png' alt=''></div>",
-"<div class='card'><img class='back-face face' src='/img/revertitparrot.gif' alt=''><img class='front-face face' src='/img/back.png' alt=''></div>",
-"<div class='card'><img class='back-face face' src='/img/revertitparrot.gif' alt=''><img class='front-face face' src='/img/back.png' alt=''></div>",
-"<div class='card'><img class='back-face face' src='/img/tripletsparrot.gif' alt=''><img class='front-face face' src='/img/back.png' alt=''></div>",
-"<div class='card'><img class='back-face face' src='/img/tripletsparrot.gif' alt=''><img class='front-face face' src='/img/back.png' alt=''></div>",
-"<div class='card'><img class='back-face face' src='/img/unicornparrot.gif' alt=''><img class='front-face face' src='/img/back.png' alt=''></div>",
-"<div class='card'><img class='back-face face' src='/img/unicornparrot.gif' alt=''><img class='front-face face' src='/img/back.png' alt=''></div>"]
-let teste = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]
-
+const cards = ["<div class='card' onclick='selecionarcard(this)'><img class='back-face face' src='/img/bobrossparrot.gif' alt=''><img class='front-face face' src='/img/back.png' alt=''></div>",
+"<div class='card' onclick='selecionarcard(this)'><img class='back-face face' src='/img/bobrossparrot.gif' alt=''><img class='front-face face' src='/img/back.png' alt=''></div>",
+"<div class='card' onclick='selecionarcard(this)'><img class='back-face face' src='/img/explodyparrot.gif' alt=''><img class='front-face face' src='/img/back.png' alt=''></div>",
+"<div class='card' onclick='selecionarcard(this)'><img class='back-face face' src='/img/explodyparrot.gif' alt=''><img class='front-face face' src='/img/back.png' alt=''></div>",
+"<div class='card' onclick='selecionarcard(this)'><img class='back-face face' src='/img/fiestaparrot.gif' alt=''><img class='front-face face' src='/img/back.png' alt=''></div>",
+"<div class='card' onclick='selecionarcard(this)'><img class='back-face face' src='/img/fiestaparrot.gif' alt=''><img class='front-face face' src='/img/back.png' alt=''></div>",
+"<div class='card' onclick='selecionarcard(this)'><img class='back-face face' src='/img/metalparrot.gif' alt=''><img class='front-face face' src='/img/back.png' alt=''></div>",
+"<div class='card' onclick='selecionarcard(this)'><img class='back-face face' src='/img/metalparrot.gif' alt=''><img class='front-face face' src='/img/back.png' alt=''></div>",
+"<div class='card' onclick='selecionarcard(this)'><img class='back-face face' src='/img/revertitparrot.gif' alt=''><img class='front-face face' src='/img/back.png' alt=''></div>",
+"<div class='card' onclick='selecionarcard(this)'><img class='back-face face' src='/img/revertitparrot.gif' alt=''><img class='front-face face' src='/img/back.png' alt=''></div>",
+"<div class='card' onclick='selecionarcard(this)'><img class='back-face face' src='/img/tripletsparrot.gif' alt=''><img class='front-face face' src='/img/back.png' alt=''></div>",
+"<div class='card' onclick='selecionarcard(this)'><img class='back-face face' src='/img/tripletsparrot.gif' alt=''><img class='front-face face' src='/img/back.png' alt=''></div>",
+"<div class='card' onclick='selecionarcard(this)'><img class='back-face face' src='/img/unicornparrot.gif' alt=''><img class='front-face face' src='/img/back.png' alt=''></div>",
+"<div class='card' onclick='selecionarcard(this)'><img class='back-face face' src='/img/unicornparrot.gif' alt=''><img class='front-face face' src='/img/back.png' alt=''></div>"]
+//usuario digita quantidade de cartas seguindo criterio descrito no prompt
 let qtdcartas = 1;
 while ((qtdcartas%2)!==0 || qtdcartas<4 || qtdcartas > 14) {
     qtdcartas = prompt("digite quantidade de cartas entre 4 e 14");
 }
-teste1 = [];
-for (i=0; teste1.length<qtdcartas;i++){
-    teste1[i]=i;
+//cria uma array com a quantidade de posições informada pelo usuário
+const randompositions = [];
+for (i=0; randompositions.length<qtdcartas;i++){
+    randompositions[i]=i;
 }
+//aplica função random na array com as possições
+randompositions.sort(comparador); 
 
-teste1.sort(comparador); // Após esta linha, a minhaArray estará embaralhada
 
-// Esta função pode ficar separada do código acima, onde você preferir
 function comparador() { 
 	return Math.random() - 0.5; 
 }
-let distcartas = [];
-for (i=0;i<teste1.length;i++){
-    distcartas[i]=cards[teste1[i]];
+
+
+//gera as cartas de acordo a ordem random 
+const selectordiv = document.querySelector('.boxcards');
+
+for (i=0;i<randompositions.length;i++){
+    selectordiv.innerHTML = selectordiv.innerHTML + cards[randompositions[i]];
 
 }
+let contclick=0;
+let x1=0;
+function selecionarcard(cardselector){
+    if(x1<2){
+    const frente = cardselector.querySelector('.front-face');
+    frente.classList.toggle('front');
 
-
-const selectordiv = document.querySelector('.boxcards');
-console.log(selectordiv);
-for (i=0;i<teste1.length;i++){
-    selectordiv.innerHTML = selectordiv.innerHTML + cards[teste1[i]];
-
+    const vers = cardselector.querySelector('.back-face');
+    vers.classList.toggle('back');
+    x1++;
+    }
 }
