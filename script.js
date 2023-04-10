7484bc661f066f8935616b1ed4c8dbba0c197761
Willyan <wkinhaamaral@gmail.com>
2023-04-10 12:27:27 -0300

    data test

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-const cards = ["<div class='card' onclick='selecionarcard(this)'><img id='1' class='back-face face' src='./img/bobrossparrot.gif' alt=''><img class='front-face face' src='./img/back.png' alt=''></div>",
-    "<div class='card' onclick='selecionarcard(this)'><img id='1' class='back-face face' src='./img/bobrossparrot.gif' alt=''><img class='front-face face' src='./img/back.png' alt=''></div>",
-    "<div class='card' onclick='selecionarcard(this)'><img id='2' class='back-face face' src='./img/explodyparrot.gif' alt=''><img class='front-face face' src='./img/back.png' alt=''></div>",
-    "<div class='card' onclick='selecionarcard(this)'><img id='2' class='back-face face' src='./img/explodyparrot.gif' alt=''><img class='front-face face' src='./img/back.png' alt=''></div>",
-    "<div class='card' onclick='selecionarcard(this)'><img id='3' class='back-face face' src='./img/fiestaparrot.gif' alt=''><img class='front-face face' src='./img/back.png' alt=''></div>",
-    "<div class='card' onclick='selecionarcard(this)'><img id='3' class='back-face face' src='./img/fiestaparrot.gif' alt=''><img class='front-face face' src='./img/back.png' alt=''></div>",
-    "<div class='card' onclick='selecionarcard(this)'><img id='4' class='back-face face' src='./img/metalparrot.gif' alt=''><img class='front-face face' src='./img/back.png' alt=''></div>",
-    "<div class='card' onclick='selecionarcard(this)'><img id='4' class='back-face face' src='./img/metalparrot.gif' alt=''><img class='front-face face' src='./img/back.png' alt=''></div>",
-    "<div class='card' onclick='selecionarcard(this)'><img id='5' class='back-face face' src='./img/revertitparrot.gif' alt=''><img class='front-face face' src='./img/back.png' alt=''></div>",
-    "<div class='card' onclick='selecionarcard(this)'><img id='5' class='back-face face' src='./img/revertitparrot.gif' alt=''><img class='front-face face' src='./img/back.png' alt=''></div>",
-    "<div class='card' onclick='selecionarcard(this)'><img id='6' class='back-face face' src='./img/tripletsparrot.gif' alt=''><img class='front-face face' src='./img/back.png' alt=''></div>",
-    "<div class='card' onclick='selecionarcard(this)'><img id='6' class='back-face face' src='./img/tripletsparrot.gif' alt=''><img class='front-face face' src='./img/back.png' alt=''></div>",
-    "<div class='card' onclick='selecionarcard(this)'><img id='7' class='back-face face' src='./img/unicornparrot.gif' alt=''><img class='front-face face' src='./img/back.png' alt=''></div>",
-    "<div class='card' onclick='selecionarcard(this)'><img id='7' class='back-face face' src='./img/unicornparrot.gif' alt=''><img class='front-face face' src='./img/back.png' alt=''></div>"]
+const cards = ["<div class='card' data-test='card' onclick='selecionarcard(this)'><img id='1' data-test='face-up-image' class='back-face face' src='./img/bobrossparrot.gif' alt=''><img class='front-face face' data-test='face-down-image'  src='./img/back.png' alt=''></div>",
+    "<div class='card' data-test='card' onclick='selecionarcard(this)'><img id='1' data-test='face-up-image' class='back-face face' src='./img/bobrossparrot.gif' alt=''><img class='front-face face' data-test='face-down-image'  src='./img/back.png' alt=''></div>",
+    "<div class='card' data-test='card' onclick='selecionarcard(this)'><img id='2' data-test='face-up-image' class='back-face face' src='./img/explodyparrot.gif' alt=''><img class='front-face face' data-test='face-down-image'  src='./img/back.png' alt=''></div>",
+    "<div class='card' data-test='card' onclick='selecionarcard(this)'><img id='2' data-test='face-up-image' class='back-face face' src='./img/explodyparrot.gif' alt=''><img class='front-face face' data-test='face-down-image'  src='./img/back.png' alt=''></div>",
+    "<div class='card' data-test='card' onclick='selecionarcard(this)'><img id='3' data-test='face-up-image' class='back-face face' src='./img/fiestaparrot.gif' alt=''><img class='front-face face' data-test='face-down-image'  src='./img/back.png' alt=''></div>",
+    "<div class='card' data-test='card' onclick='selecionarcard(this)'><img id='3' data-test='face-up-image' class='back-face face' src='./img/fiestaparrot.gif' alt=''><img class='front-face face' data-test='face-down-image'  src='./img/back.png' alt=''></div>",
+    "<div class='card' data-test='card' onclick='selecionarcard(this)'><img id='4' data-test='face-up-image' class='back-face face' src='./img/metalparrot.gif' alt=''><img class='front-face face' data-test='face-down-image'  src='./img/back.png' alt=''></div>",
+    "<div class='card' data-test='card' onclick='selecionarcard(this)'><img id='4' data-test='face-up-image' class='back-face face' src='./img/metalparrot.gif' alt=''><img class='front-face face' data-test='face-down-image'  src='./img/back.png' alt=''></div>",
+    "<div class='card' data-test='card' onclick='selecionarcard(this)'><img id='5' data-test='face-up-image' class='back-face face' src='./img/revertitparrot.gif' alt=''><img class='front-face face' data-test='face-down-image'  src='./img/back.png' alt=''></div>",
+    "<div class='card' data-test='card' onclick='selecionarcard(this)'><img id='5' data-test='face-up-image' class='back-face face' src='./img/revertitparrot.gif' alt=''><img class='front-face face' data-test='face-down-image'  src='./img/back.png' alt=''></div>",
+    "<div class='card' data-test='card' onclick='selecionarcard(this)'><img id='6' data-test='face-up-image' class='back-face face' src='./img/tripletsparrot.gif' alt=''><img class='front-face face' data-test='face-down-image'  src='./img/back.png' alt=''></div>",
+    "<div class='card' data-test='card' onclick='selecionarcard(this)'><img id='6' data-test='face-up-image' class='back-face face' src='./img/tripletsparrot.gif' alt=''><img class='front-face face' data-test='face-down-image'  src='./img/back.png' alt=''></div>",
+    "<div class='card' data-test='card' onclick='selecionarcard(this)'><img id='7' data-test='face-up-image' class='back-face face' src='./img/unicornparrot.gif' alt=''><img class='front-face face' data-test='face-down-image'  src='./img/back.png' alt=''></div>",
+    "<div class='card' data-test='card' onclick='selecionarcard(this)'><img id='7' data-test='face-up-image' class='back-face face' src='./img/unicornparrot.gif' alt=''><img class='front-face face' data-test='face-down-image'  src='./img/back.png' alt=''></div>"]
 //usuario digita quantidade de cartas seguindo criterio descrito no prompt
 let qtdcartas = 1;
 while ((qtdcartas % 2) !== 0 || qtdcartas < 4 || qtdcartas > 14) {
     qtdcartas = prompt("digite quantidade de cartas entre 4 e 14");
 }
+qtdcartas1 = Number(qtdcartas);
 //cria uma array com a quantidade de posições informada pelo usuário
 const randompositions = [];
-for (i = 0; randompositions.length < qtdcartas; i++) {
+for (i = 0; randompositions.length < qtdcartas1; i++) {
     randompositions[i] = i;
 }
 //aplica função random na array com as possições
@@ -39,6 +40,7 @@ for (i = 0; i < randompositions.length; i++) {
 
 }
 let contclick = 0;
+let match=0;
 let x1 = 0;
 let c1f, c1b, c2f, c2b, c1id, c2id;
 
@@ -70,6 +72,10 @@ function selecionarcard(cardselector) {
         if (c1id === c2id) {
             x1 = 0;
             contclick +=2;
+            match+=2;
+            if(match===qtdcartas1){
+                setTimeout(mens, 1000);
+            }
 
 
         }
@@ -92,4 +98,11 @@ function selecionarcard(cardselector) {
 
 
     }
+}
+
+
+
+function mens(){
+    alert('Você ganhou em '+contclick+ 'jogadas!');
+
 }
